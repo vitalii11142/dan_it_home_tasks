46731f44cbf45d726569dbc889e6a22e2efc8b39
Vitaliy <vitaliysevba8@gmail.com>
2025-07-21 15:10:17 +0300

    home work 1

---
+++ hometasks/src/main/java/java_hw_1/Program.java
@@ -0,0 +1,13 @@
+package java_hw_1;
+
+public class Program {
+            public static void main(String[] args) {
+                int a = 7;
+                int b = 9;
+                a = a + b;
+                b = a - b;
+                a = a - b;
+                System.out.println("a = " + a);
+                System.out.println("b = " + b);
+            }
+        }
